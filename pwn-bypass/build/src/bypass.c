d9e6d09a2eca4a6844b115b1ceb8356cc78e6357
ziran <17888368052@163.com>
2025-12-20 12:38:14 +0800

    Update bypass.c

--- pwn-bypass/build/src/bypass.c
+++ pwn-bypass/build/src/bypass.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-
+#include <string.h>
+#
 void backdoor(){
     printf("backdoor called\n");
     system("/bin/sh");
@@ -13,10 +13,24 @@ void vuln(){
     printf("chunk_addr: %p\n",p);
     read(0,p,0x1000);
     memcpy(buf,p,0x100);
+    
     free(p);
 }
 
+void init(){
+    setvbuf(stdout,0,2,0);
+    setvbuf(stdin,0,2,0);
+    setvbuf(stderr,0,2,0);
+}
+void gift() {
+    __asm__(
+        "pop %rdi\n"
+        "ret\n"
+    );
+}
+
 int main(){
+    init();
     char buf[0x40];
     char *p = malloc(0x100);
     puts("input your name:");
