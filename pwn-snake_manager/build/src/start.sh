931c50423a3531d536c9598f34c9c0d01a2be8e4
ziran <17888368052@163.com>
2026-03-28 17:57:57 +0800

    Refactor flag handling and improve script execution

--- pwn-snake_manager/build/src/start.sh
+++ pwn-snake_manager/build/src/start.sh
@@ -1,25 +1,32 @@
 #!/bin/sh
+set -eu
 
-if [ "$A1CTF_FLAG" ]; then
+if [ "${A1CTF_FLAG:-}" ]; then
     INSERT_FLAG="$A1CTF_FLAG"
     unset A1CTF_FLAG
-elif [ "$PCTF_FLAG" ]; then
+elif [ "${PCTF_FLAG:-}" ]; then
     INSERT_FLAG="$PCTF_FLAG"
     unset PCTF_FLAG
-elif [ "$GZCTF_FLAG" ]; then
+elif [ "${GZCTF_FLAG:-}" ]; then
     INSERT_FLAG="$GZCTF_FLAG"
     unset GZCTF_FLAG
-elif [ "$FLAG" ]; then
+elif [ "${FLAG:-}" ]; then
     INSERT_FLAG="$FLAG"
     unset FLAG
 else
     INSERT_FLAG="PCTF{!!!!_FLAG_ERROR_ASK_ADMIN_!!!!}"
 fi
 
-echo -n $INSERT_FLAG > /home/ctf/flag
-INSERT_FLAG=""
+printf '%s' "$INSERT_FLAG" > /home/ctf/flag
+unset INSERT_FLAG
 chown ctf:ctf /home/ctf/flag
 
-cp /bin/sh /home/ctf/sh && chmod +x /home/ctf/sh
+cp /bin/sh /home/ctf/sh
+chmod +x /home/ctf/sh
 
-socat -T60 TCP-LISTEN:8000,reuseaddr,fork EXEC:"/usr/sbin/chroot /home/ctf ./pwn",stderr
+CHROOT_BIN="/usr/sbin/chroot"
+if [ ! -x "$CHROOT_BIN" ]; then
+    CHROOT_BIN="/usr/bin/chroot"
+fi
+
+exec socat -T60 TCP-LISTEN:8000,reuseaddr,fork EXEC:"$CHROOT_BIN /home/ctf ./pwn",stderr
