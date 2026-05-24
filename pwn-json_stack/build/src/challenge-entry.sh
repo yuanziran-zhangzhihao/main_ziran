8c3337ea00c7cb8c19793e11d490fe5bcef18558
ziranan <17888368052@163.com>
2026-05-24 13:41:47 +0800

    Fix local container build/runtime

--- pwn-json_stack/build/src/challenge-entry.sh
+++ pwn-json_stack/build/src/challenge-entry.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 set -eu
 
+CHROOT_BIN="/usr/sbin/chroot"
+if [ ! -x "$CHROOT_BIN" ]; then
+    CHROOT_BIN="/usr/bin/chroot"
+fi
+
+CTF_UID="$(id -u ctf)"
+CTF_GID="$(id -g ctf)"
+
 # Drop inherited extra descriptors so the remote process sees a cleaner runtime.
 exec 3>&- 4>&- 5>&- 6>&- 7>&- 8>&- 9>&-
-exec /usr/sbin/chroot --userspec=ctf:ctf /home/ctf /pwn
+exec "$CHROOT_BIN" --userspec="${CTF_UID}:${CTF_GID}" /home/ctf /pwn
