dc3288160fbf52f86761f81c864fc26b625e55f7
ziranan <17888368052@163.com>
2026-05-24 13:40:38 +0800

    Fix local container build/runtime

--- pwn-aespwn/build/src/challenge-entry.sh
+++ pwn-aespwn/build/src/challenge-entry.sh
@@ -6,5 +6,8 @@ if [ ! -x "$CHROOT_BIN" ]; then
     CHROOT_BIN="/usr/bin/chroot"
 fi
 
+CTF_UID="$(id -u ctf)"
+CTF_GID="$(id -g ctf)"
+
 exec 3>&- 4>&- 5>&- 6>&- 7>&- 8>&- 9>&-
-exec "$CHROOT_BIN" --userspec=ctf:ctf /home/ctf /pwn
+exec "$CHROOT_BIN" --userspec="${CTF_UID}:${CTF_GID}" /home/ctf /pwn
