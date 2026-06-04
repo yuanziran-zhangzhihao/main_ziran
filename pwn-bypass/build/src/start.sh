4974e4b8976a2a298f3b744b51c6060321a26dee
ziranan <17888368052@163.com>
2026-06-04 19:25:56 +0800

    Set challenge-specific static flag

--- pwn-bypass/build/src/start.sh
+++ pwn-bypass/build/src/start.sh
@@ -14,7 +14,7 @@ elif [ "${FLAG:-}" ]; then
     INSERT_FLAG="$FLAG"
     unset FLAG
 else
-    INSERT_FLAG="PCTF{pwn_bypass_static_20260604}"
+    INSERT_FLAG="PCTF{bypass_login_heap_pointer_ret2backdoor}"
 fi
 
 printf '%s' "$INSERT_FLAG" > /home/ctf/flag
