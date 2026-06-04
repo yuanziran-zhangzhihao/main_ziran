6cacf44f5934e8c113c09fa88257a8d07289e492
ziranan <17888368052@163.com>
2026-06-04 19:25:56 +0800

    Set challenge-specific static flag

--- pwn-snake_manager/build/src/start.sh
+++ pwn-snake_manager/build/src/start.sh
@@ -16,7 +16,7 @@ elif [ "${FLAG:-}" ]; then
     INSERT_FLAG="$FLAG"
     unset FLAG
 else
-    INSERT_FLAG="PCTF{pwn_snake_manager_static_20260604}"
+    INSERT_FLAG="PCTF{snake_manager_password_leak_admin_shell}"
 fi
 
 printf '%s' "$INSERT_FLAG" > /home/ctf/flag
