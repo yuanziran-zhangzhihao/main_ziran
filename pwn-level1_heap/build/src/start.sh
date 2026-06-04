e559dd66b5b7d2c7762349b278753adf435e5b74
ziranan <17888368052@163.com>
2026-06-04 19:25:56 +0800

    Set challenge-specific static flag

--- pwn-level1_heap/build/src/start.sh
+++ pwn-level1_heap/build/src/start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -eu
 
-FLAG_VALUE="PCTF{pwn_level1_heap_static_20260604}"
+FLAG_VALUE="PCTF{heap_level1_fastbin_dup_to_stack_rop}"
 CTF_UID="${CTF_UID:-1000}"
 CTF_GID="${CTF_GID:-1000}"
 
