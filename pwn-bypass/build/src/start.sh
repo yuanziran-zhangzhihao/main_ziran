f23518e58d812f7d00c6de0c89bbdda40699d4cb
ziranan <17888368052@163.com>
2026-06-04 19:02:02 +0800

    Set pwn-bypass static fallback flag

--- pwn-bypass/build/src/start.sh
+++ pwn-bypass/build/src/start.sh
@@ -14,7 +14,7 @@ elif [ "${FLAG:-}" ]; then
     INSERT_FLAG="$FLAG"
     unset FLAG
 else
-    INSERT_FLAG="PCTF{!!!!_FLAG_ERROR_ASK_ADMIN_!!!!}"
+    INSERT_FLAG="PCTF{pwn_bypass_static_20260604}"
 fi
 
 printf '%s' "$INSERT_FLAG" > /home/ctf/flag
